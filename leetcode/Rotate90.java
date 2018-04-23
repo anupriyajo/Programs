646ebc66cbdc2c7704adc27598e9d8dba9e01fa3
anupriya johari <anupriya.johari@wipro.com>
2018-04-23 16:22:40 +0100

    90 rotation

--- leetcode/Rotate90.java
+++ leetcode/Rotate90.java
@@ -3,23 +3,21 @@ package leetcode;
 public class Rotate90 {
     public static void main(String args[]) {
         int a[][] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
-        //printArr(a);
+        printArr(a);
         a = rotateMatrix(a);
-        //printArr(a);
+        printArr(a);
 
     }
 
     private static int[][] rotateMatrix(int[][] a) {
         int length = a.length;
-        for (int x = 0; x < 1; x++) {
+        for (int x = 0; x < length / 2; x++) {
             for (int y = x; y < length - x - 1; y++) {
-                System.out.println("X: " + x + "Y: " + y);
                 int tmp = a[x][y];//save top
                 a[x][y] = a[length - 1 - y][x];// left to top
-                //a[length - 1 - y][x] = a[length - 1 - x][length - 1 - y];//bottom to left
-                //a[length - 1 - x][length - 1 - y] = a[y][x];//right to bottom
-                //a[y][x] = tmp; // top to right
-                printArr(a);
+                a[length - 1 - y][x] = a[length - 1 - x][length - 1 - y];//bottom to left
+                a[length - 1 - x][length - 1 - y] = a[y][length - 1 - x];//right to bottom
+                a[y][length - 1 - x] = tmp; // top to right
             }
         }
         return a;
